ba28fcd7d01090db25e474325adb954406f1855b
severn2 <bondie.wes@gmail.com>
2020-05-25 17:07:55 -0500

    victory trigger

---
+++ Assets/Scripts/VictoryTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryTrigger : MonoBehaviour
+{
+    public GameObject victoryMenu;
+
+    public void Awake()
+    {
+        victoryMenu.SetActive(false);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            victoryMenu.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
