826b79dd928b5c64947c051b7b6c6409755c2218
leohlim <LEOHLIM@GMAIL.COM>
2020-05-27 19:31:25 -0500

    Added basic script for killing and respawning player

---
+++ Assets/DeathRespawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathRespawn : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController playerController;
+    [SerializeField]
+    private Transform Player;
+    [SerializeField]
+    private Transform respawnPoint;
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Triggered");
+        playerController.enabled = false;
+        Player.transform.position = respawnPoint.transform.position;
+        playerController.enabled = true;
+    }
+}
